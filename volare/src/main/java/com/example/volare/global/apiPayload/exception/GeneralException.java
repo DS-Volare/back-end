d4ee5dfac65ebcb5ba99897ca42e3d50397cda9e
hyesoo kim <khwater1017@naver.com>
2024-06-30 01:17:27 +0900

    [init] Exception handling

---
+++ volare/src/main/java/com/example/volare/global/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.example.volare.global.apiPayload.exception;
+
+import com.example.volare.global.apiPayload.code.BaseErrorCode;
+import com.example.volare.global.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
