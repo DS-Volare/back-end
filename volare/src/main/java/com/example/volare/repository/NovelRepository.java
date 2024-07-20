883ca567c7843f21802ae6ba2aec02bb34f2d805
hyesoo kim <khwater1017@naver.com>
2024-07-21 06:51:12 +0900

    [feat] Add PathVariable- requesting novel information

--- volare/src/main/java/com/example/volare/repository/NovelRepository.java
+++ volare/src/main/java/com/example/volare/repository/NovelRepository.java
@@ -3,5 +3,8 @@ package com.example.volare.repository;
 import com.example.volare.model.Novel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NovelRepository extends JpaRepository<Novel,String> {
+    Optional<Novel> findById(String novelId);
 }
