576d747a6b82c752c7513bc60e61bafb966df921
hyesoo kim <khwater1017@naver.com>
2024-08-02 00:16:30 +0900

    [fix] Modify Return Token Location (header -> cookie)
    FE에서 응답헤더에 접근하지 못하는 문제 상황 발생: 3xx 리다이렉션 시 응답헤더, 응답바디 접근 못함, 이에 쿠기로 토큰을 전달하도록 수정
    
    Github issue #6 #8

--- volare/src/main/java/com/example/volare/global/common/auth/CustomAuthenticationSuccessHandler.java
+++ volare/src/main/java/com/example/volare/global/common/auth/CustomAuthenticationSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.example.volare.global.common.auth;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -17,16 +18,26 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     private final HttpSession httpSession;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws  ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         // 세션에서 토큰 가져오기
         String accessToken = (String) httpSession.getAttribute("accessToken");
         String refreshToken = (String) httpSession.getAttribute("refreshToken");
 
-        // 응답 설정
-        response.setCharacterEncoding("utf-8");
-        response.setHeader("X-AUTH-TOKEN", accessToken);
-        response.setHeader("refresh-token", refreshToken);
-        response.setContentType("application/json");
 
+        // 쿠키 설정
+        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        accessTokenCookie.setPath("/");
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setPath("/");
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+
+        // 리다이렉트 URL 설정
+        String redirectUri = "http://localhost:3000";
+
+        // 리다이렉트 수행
+        response.sendRedirect(redirectUri);
     }
 }
